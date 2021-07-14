5fb2d8a8bbf251c8e3fab9d915014a74ddcb2cfb
Alfath Noverio <alfathnv@gmail.com>
2021-07-14 08:42:06 +0700

    updata App.js dengan import Playlist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import {Playlist} from './components/Playlist';
 
 function App() {
-  const API_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-  const API_KEY = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Playlist></Playlist>
       </header>
     </div>
   );
