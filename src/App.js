9cdbb7db71567ed6993cec2d79424873aff01d25
Alfath Noverio <alfathnv@gmail.com>
2021-07-17 23:43:22 +0700

    update homework 0.2.3

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import { Playlist } from './containers/pages/playlist';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Playlist></Playlist>
-      </header>
     </div>
   );
 }
