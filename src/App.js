ddaa72b6c4bda1485de0f01d1bf0d993003dc8c2
Alfath Noverio <alfathnv@gmail.com>
2021-07-28 15:57:03 +0700

    finally i can do 0.3.2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import Login from './components/Login'
 import Playlist from './components/Playlist'
+
 function App() {
-    let page
-    console.log(window.location.href)
-    if (window.location.href === 'http://localhost:3000/') {
-        page = <Login />
-    } else {
-        page = <Playlist />
-    }
-    return <div>{page}</div>
+    return <Playlist></Playlist>
 }
 
 export default App
